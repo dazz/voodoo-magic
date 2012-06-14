033e3abc7320210bb1df57fb2c24bdfb1289eac2
Brian Wiborg <baccenfutter@c-base.org>
2012-06-14 22:54:37 +0200

    adding spooling-functions.sh

---
+++ usr/lib/voodoo-magic/spooling-functions.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# Voodoo-Magic
+#
+#    Voodoo-Magic is free software; you can redistribute it and/or modify it
+#    under the terms of the GNU General Public License as published by the Free
+#    Software Foundation; either version 2 of the License, or (at your option)
+#    any later version.
+#
+#    Voodoo-Magic is distributed in the hope that it will be useful, but
+#    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+#    or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+#    for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with Voodoo-Magic; if not, write to the Free Software Foundation,
+#    Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+AddSpoolFile() {
+    local tmpfile="$(mktemp --tmpdir="$SPOOL_DIR" "$PROGRAM".XXXXXX)"
+    AddExitTask "rm \"$tmpfile\""
+    echo $tmpfile
+}
+
+RemoveSpoolFile() {
+    [[ -f "$1" ]] && \
+        [[ "$1" == *"$SPOOL_DIR"* ]] && \
+        RemoveExitTask "rm \"$tmpfile\"" && \
+        rm "$tmpfile"
+    StopIfError "Can not remove spoolfile: $tmpfile"
+}
+
+AddFiFo() {
+    local tmpfile="$(mktemp --tmpdir="$SPOOL_DIR" "$PROGRAM".fifo.XXXXXX)"
+    local fifo="$(mkfifo "$tmpfile")"
+    AddExitTask "rm \"$fifo\""
+    echo $fifo
+}
+
+RemoveFiFo() {
+    [[ -p "$1" ]] && \
+        [[ "$1" == *"$SPOOL_DIR"* ]] && \
+        RemoveExitTask "rm \"$fifo\"" && \
+        rm "$fifo"
+    StopIfError "Can not remove fifo: $fifo"
+}
+
