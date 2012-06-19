9185cf759fa0faa571ad689dc6300cd0ab96e4b6
Brian Wiborg <baccenfutter@c-base.org>
2012-06-19 23:22:22 +0200

    splitting 'new' command into functions

--- usr/share/voodoo-magic/workflows/workflow/functions/01_new-functions.sh
+++ usr/share/voodoo-magic/workflows/workflow/functions/01_new-functions.sh
@@ -17,3 +17,37 @@
 #    Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 AddArg 'name:' 'wf_name=$2'
+
+CreateWorkflowDirectories() {
+    # create the directories
+    LogPrint "Creating new workflow: $wf_name"
+    directories=( "$wf_dir" "$wf_dir/"{conf,doc,functions,tmp} )
+    files=(
+        "$wf_dir/conf/$wf_name.conf"
+        "$wf_dir/doc/"{description,usage}
+        "$wf_dir/functions/00_general.sh"
+        "$wf_dir/workflow_$wf_name"
+    )
+    LogPrint "Creating workflow directories"
+    for d in "${directories[@]}"; do
+        install -d "$d"
+    done
+}
+
+CreateWorkflowFiles() {
+    # create files by writing the GNU/GPL header in each file
+    LogPrint "Creating workflow files"
+    for f in "${files[@]}"; do
+        head -n 18 $0 > "$f"
+    done
+}
+
+AddWorkflowSkel() {
+    # add the workflow function skel
+    LogPrint "Adding workflow function"
+    echo "WORKFLOW_$wf_name() {
+        
+    }
+    " >> "$wf_dir/workflow_$wf_name"
+}
+
